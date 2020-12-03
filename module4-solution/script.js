5d67b4abf23fffeb312e5be09ed860e233c46c63
pedronajera93 <pedroluisnajera@gmail.com>
2020-12-02 23:30:52 -0600

    finished
    done with changes. code working

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,4 +1,4 @@
-// ******************************* 
+// *******************9+************* 
 // START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
 //
